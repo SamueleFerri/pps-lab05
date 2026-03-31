e3df78a01385e4eb9afa317bf07b52a872722967
Samuele Ferri <samuele1301@gmail.com>
2026-03-31 14:01:25 +0200

    feat: LogicsImpl.scala a01a done

--- src/main/scala/it/unibo/pps/polyglot/a01a/LogicsImpl.scala
+++ src/main/scala/it/unibo/pps/polyglot/a01a/LogicsImpl.scala
@@ -1,10 +1,30 @@
 package it.unibo.pps.polyglot.a01a
 
-import it.unibo.pps.polyglot.a01a.Logics
-import it.unibo.pps.polyglot.a01a.Logics.Result
+import scala.util.Random
+
+enum Result:
+  case HIT, MISS, WON, LOST
 
 /** solution and descriptions at https://bitbucket.org/mviroli/oop2019-esami/src/master/a01a/sol2/ */
-class LogicsImpl(private val size: Int, private val boat: Int) extends Logics:
+trait NewLogics:
+  def hit(row: Int, col: Int): Result
 
-  def hit(row: Int, col: Int) =
-    Result.HIT
+class LogicsImpl(private val size: Int, private val boat: Int) extends NewLogics:
+
+  private val random = Random(42)
+  private var missedShot = 0
+  private var hitShot = 0
+  private val boatRow = random.nextInt(size)
+  private val boatStartCol = random.nextInt(size - boat + 1)
+
+  println(s"DEBUG - Boat row: $boatRow, col: $boatStartCol")
+
+  override def hit(row: Int, col: Int): Result = {
+    if row == boatRow && boatStartCol + boat > col && col >= boatStartCol then
+      hitShot = hitShot + 1
+      if hitShot == boat then Result.WON else Result.HIT
+    else
+      missedShot = missedShot + 1
+      if missedShot == 5 then Result.LOST else Result.MISS
+  }
+
