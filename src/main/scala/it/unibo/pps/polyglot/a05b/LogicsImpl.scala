3ca0ca32e1e99434fae70489d904554c1caf74f4
Samuele Ferri <samuele1301@gmail.com>
2026-03-31 10:27:12 +0200

    feat: LogicsImpl.scala done

--- src/main/scala/it/unibo/pps/polyglot/a05b/LogicsImpl.scala
+++ src/main/scala/it/unibo/pps/polyglot/a05b/LogicsImpl.scala
@@ -1,11 +1,28 @@
 package it.unibo.pps.polyglot.a05b
 
 import it.unibo.pps.polyglot.a05b.Logics
+import it.unibo.pps.util.Optionals.Optional
+import it.unibo.pps.util.Optionals.Optional.*
+import scala.util.Random
 
 /** solution and descriptions at https://bitbucket.org/mviroli/oop2019-esami/src/master/a05b/sol2/ */
 class LogicsImpl(private val size: Int) extends Logics:
-  override def tick(): Unit = {}
 
-  override def isOver: Boolean = false
+  private var center: Optional[(Int, Int)] = Empty()
+  private var steps: Int = 0
+  private val random = Random(42)
 
-  override def hasElement(x: Int, y: Int): Boolean = false
+  override def tick(): Unit = center match
+    case Empty() => center = Just((random.nextInt(size), random.nextInt(size)))
+    case Just(_) => steps = steps + 1
+
+  override def isOver: Boolean = center match
+    case Empty() => false
+    case Just((x, y)) => x - steps < 0 || x + steps >= size || y - steps < 0 || y + steps >= size
+
+  override def hasElement(x: Int, y: Int): Boolean = center match
+    case Empty() => false
+    case Just((cx, cy)) =>
+      val isOnRay = (x == cx) || (y == cy) || (math.abs(cx - x) == math.abs(cy - y))
+      val isWithinSteps = math.max(math.abs(cx - x), math.abs(cy - y)) <= steps
+      isOnRay && isWithinSteps
